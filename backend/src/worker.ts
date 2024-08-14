2a28c36503aecd5112deabe62a3ec7127f812405
Elementttt <121469830+MattCarneiro@users.noreply.github.com>
2024-08-14 03:45:08 -0300

    Update worker.ts

--- backend/src/worker.ts
+++ backend/src/worker.ts
@@ -1,1 +1,8 @@
+// src/worker.ts
 
+import { startWorker } from './libs/rabbitmq';
+import { handleMessage } from './services/WbotServices/wbotMessageListener';
+import { verifyRecentCampaign, verifyCampaignMessageAndCloseTicket } from './services/WbotServices/wbotMessageListener';
+import { getWbot } from './libs/wbot'; // Ajuste o caminho conforme necessário
+
+startWorker(handleMessage, getWbot, verifyRecentCampaign, verifyCampaignMessageAndCloseTicket);
